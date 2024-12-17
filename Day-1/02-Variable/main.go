a7b22c8afd6a4eb7a000c1e78d7f91c53d7b4ac0
ujjwal-dahal <toppelp@gmail.com>
2024-12-17 12:34:46 +0545

    Learned about Variable & Private , Public Variable

---
+++ Day-1/02-Variable/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// using "var" keyword -> "var" keyword use garda hamile datatype nadida ni bhayo
+
+	// var userName = "Ujjwal"
+	// fmt.Println("Your Name is : ",userName)
+
+	//using "var" keyword -> But Hamile Data Type dina ni milcha For Type Safety
+	// var userName string = "Hari"
+	// fmt.Println("Your Name is : ", userName)
+
+	//Without Using "var" keyword => insted of "=" we have to use ":="
+	// userName := "My Name"
+	// fmt.Println("Your Name is : ",userName)
+	// fmt.Printf("Data Type is %T", userName)
+}
